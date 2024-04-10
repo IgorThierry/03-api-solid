1181bdf94e3f9d049406a52ea1afb3e3e71b93f4
Igor Thierry <igorthierry15@gmail.com>
2024-04-10 18:23:12 -0300

    Gerando coverage com vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,16 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      exclude: [
+        '**/build/**',
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      ],
+    },
+  },
 })
