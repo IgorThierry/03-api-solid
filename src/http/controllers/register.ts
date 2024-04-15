9c452917242eb7e9947efd84962bdbb16c3efb54
Igor Thierry <igorthierry15@gmail.com>
2024-04-15 10:37:35 -0300

    Teste e controller de autenticação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repositories'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
+import { authenticate } from '@/http/controllers/authenticate'
 import { register } from './controllers/register'
 
 export async function appRoute(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
